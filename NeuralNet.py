d601270fbcb5d133dd37ac197e1cb8b8c37468da
Felipe <felipeacx@yahoo.com.br>
2018-09-30 13:56:30 +0200

    Working neural network

--- NeuralNet.py
+++ NeuralNet.py
@@ -6,17 +6,14 @@ class NeuralNet:
         self.inputs = layers[0] # int
         self.outputs = layers[1] # int
         self.length = len(layers)# int
-        print(self.length)
         self.nodes = []
         self.error = []
         for _ in range(self.length - 1):
             self.nodes.append(1)
             self.error.append(1)
-        self.learningRate = 0.5
+        self.learningRate = 1
         self.bias = [np.random.randn(y, 1) for y in layers[1:]]
-        self.weights = [np.random.randn(y, x) for x, y in zip(layers[:-1], layers[1:])]
-        #print(self.bias)
-        #print(self.weights)        
+        self.weights = [np.random.randn(y, x) for x, y in zip(layers[:-1], layers[1:])]     
           
     # Starts with a rate of 0.5 but it can be changed using this function
     def setLearningRate(self, rate):
@@ -37,57 +34,64 @@ class NeuralNet:
         
         # Calculates the result of each multiplication and stores it into a new vector
         for i in range(self.length - 1):
+            # print("self.nodes: " + str(input))
             self.nodes[i] = self.sigmoid(np.dot(self.weights[i], input) + self.bias[i])
             input = self.nodes[i]
+            
+        return self.nodes[-1]
 
     def train(self, input, target):
-        # (output - target) --> finds the difference between what 
-        outputError = (self.nodes[-1] - np.asarray(target).reshape(len(target),1))
+        out = self.feedForward(input)
+        # (output - target) --> finds the difference between what we want and what it calculated
+        outputError = (np.asarray(target).reshape(len(target),1) - out)
         input = np.asarray(input).reshape(len(input),1)
         for i in range(self.length - 1):
             # Apply the derivative of sigmoid to 
-            dydz = self.dsigmoid(self.nodes[self.length - 2 - i])
-            # dzdW = np.dot(self.weights[-1], self.nodes[-2]) 
+            dydz = self.dsigmoid(self.nodes[self.length-2-i])
             if i is 0:
+                # Hadamard product of the error in the guess and the dsigmoid of the output
+                delta = np.multiply(dydz, outputError)
+                # Matrix multiplication of the delta and input of that weight (the node before the last node)
+                self.error[i] = self.learningRate * np.dot(delta, self.nodes[-2].T)         
+                self.bias[self.length-2-i] += delta  
+            elif i is self.length-2:
                 # Hadamard product of the error in guess and the dsigmoid of the output
-                delta3 = np.multiply(outputError, dydz)
-                print("delta: " + str(delta3.shape))
-                # Matrix multiplication of the delta and the value in eah node of that layer
-                self.error[i] = np.dot(delta3, self.nodes[self.length - 3 - i].T)
-            elif i is self.length - 2:
-                print("delta: " + str(delta3.shape))
-                print("dzdW: " + str(self.weights[0].shape))
-                delta3 = np.dot(self.weights[0], delta3) * self.dsigmoid(dydz)
-                self.error[i] = np.dot(delta3, input.T)
+                outputError = np.dot(self.weights[self.length-1-i].T, outputError)
+                delta = np.multiply(dydz, outputError) 
+                self.error[i] = self.learningRate * np.dot(delta, input.T) 
+                self.bias[self.length-2-i] += delta
             else:
-                return
+                outputError = np.dot(self.weights[self.length-1-i].T, outputError)
+                delta = np.multiply(dydz, outputError) 
+                self.error[i] = self.learningRate * np.dot(delta, self.nodes[self.length-2-i].T) 
+                self.bias[self.length-2-i] += delta
+        self.update()
 
-        print("error: "+ str(self.error))
-        print("weight: "+ str(self.weights))
+        return outputError
 
-        
+    def update(self):
+        for i in range(self.length - 1):
+                self.weights[i] += self.error[self.length-2-i]
 
+            
 # ---------------------------------------------------------
 # ---------------------------------------------------------
 # ---------------------------------------------------------
-n = NeuralNet([2, 3, 2])
-'''der = [{"input":[0,0], "target":[0]},
+n = NeuralNet([2, 2, 1])
+der = [{"input":[0,0], "target":[0]},
         {"input":[0,1], "target":[1]},
         {"input":[1,0], "target":[1]},
         {"input":[1,1], "target":[0]}]
-        
-for i in range(1000):
+#print("weight: "+ str(n.weights))
+
+for i in range(10000):
     a = np.random.randint(0,4)
-    # print(der[a]["input"] + der[a]["target"])
+    #print(der[a]["input"] + der[a]["target"])
     n.train(der[a]["input"], der[a]["target"])
+
 
 print(n.feedForward([0,0]))
 print(n.feedForward([0,1]))
 print(n.feedForward([1,0]))
 print(n.feedForward([1,1]))
-
-print("Input: " + str(n.inputWeights))
-print("Hidden: " + str(n.hiddenWeights))
-print("Ouput: " + str(n.outputWeights))'''
-n.feedForward([1, 1])
-n.train([1, 1], [0, 1])
+#print("weight: "+ str(n.weights))
